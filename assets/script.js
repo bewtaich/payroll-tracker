296e7d80f0c819790341d5fd8db065caeb19ff66
Bewtaich <bewtaich@gmail.com>
2024-07-05 09:46:56 -0500

    Added user input for employee data the returns in table

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,29 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
-
+const employeeArray = [];
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
+  const firstName = prompt ("What is the employee's first name?");
+  const lastName = prompt (`What is ${firstName}'s last name?`);
+  const salary = prompt (`What is ${firstName} ${lastName}'s salary?`);
+
+    const currentEmployee = {
+      firstName: firstName,
+      lastName: lastName,
+        salary: salary}
+      
+        employeeArray.push(currentEmployee);
+
+  
+        return (employeeArray);
+
 }
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
+
 }
 
 // Select a random employee
